2cbce732b1fbc3da69a8df9c21d04750e7e8f62d
ColinM <mozzbuilds@gmail.com>
2025-08-14 19:25:41 +0100

    First commit

---
+++ Beezneez/BeezneezApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeezneezApp.swift
+//  Beezneez
+//
+//  Created by Colin Morrison on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct BeezneezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
